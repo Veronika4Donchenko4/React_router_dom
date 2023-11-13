66e77a8ab89843958be6d47213f1605903f50a06
Nika Donchenko <donchenko.veronika96@gmail.com>
2023-11-14 00:12:09 +0100

    created main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">Future project</div>;
+      <div className="App">
+        <Routes>
+          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path="contacts" element={<h1>Contacts</h1>} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
