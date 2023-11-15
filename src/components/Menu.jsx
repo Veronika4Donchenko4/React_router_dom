c72f5547d99e3de7dbbf66f83375fcae4e0d699b
Nika Donchenko <donchenko.veronika96@gmail.com>
2023-11-15 20:56:23 +0100

    added page about courses

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,12 @@ import { NavLink } from 'react-router-dom';
 const Menu = () => {
   return (
     <nav>
-      <NavLink to=".">Home</NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? 'activeLink' : 'link')}
+        to="."
+      >
+        Home
+      </NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/contacts">Contacts</NavLink>
     </nav>
---
+++ src/data/courses.js
@@ -0,0 +1,22 @@
+const courses = [
+  {
+    title: 'Complete Js Guide',
+    slug: 'js',
+    id: 257,
+  },
+  {
+    title: 'Complete React Guide',
+    slug: 'react',
+    id: 876,
+  },
+  {
+    title: 'Complete Node.js Guide',
+    slug: 'node-js',
+    id: 285,
+  },
+  {
+    title: 'Complete Python Guide',
+    slug: 'python',
+    id: 298,
+  },
+];
